79ddc5aae76ba0b2fd3ffa20ba7029689f39e7e5
seon <seonu0823@gmail.com>
2019-04-28 11:24:49 +0900

    #4-1 GlobalStyles and Theme

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BORDER = '1px solid #E6E6E6';
+const BORDER_RADIUS = '4px';
+
+export default {
+    bgColor : '#FAFAFA',
+    blackColor : '#262626',
+    blueColor : '#3897F0',
+    darkGreyColor : '#999',
+    lightGreyColor : '#C7C7C7',
+    redColor : '#ED4956',
+    darkBlueColor : '#003569',
+    boxBorder : '1px solid #E6E6E6',
+    borderRadius : '4px',
+    whiteBox : `${ BORDER_RADIUS };
+                ${ BOX_BORDER };
+                background-color : white;
+                `
+};
